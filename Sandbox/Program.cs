febf790554adf0affe8b99413818cb00c899fff0
libertyernie <maybeway36@gmail.com>
2016-10-10 15:56:49 -0500

    Add "unsafe" code to read comments from ogg file

---
+++ Sandbox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using VorbisCommentSharp;
+
+namespace Sandbox {
+    class Program {
+        unsafe static void Main(string[] args) {
+            byte[] b = File.ReadAllBytes(@"test.logg");
+            fixed (byte* start = b) {
+                byte* end = start + b.Length;
+                List<VorbisHeader> list = VorbisHeader.Search(start, end);
+                foreach (var h in list) {
+                    Console.WriteLine(h.PacketType);
+                    if (h.PacketType == 3) {
+                        var c = h.ExtractComments();
+                        Console.WriteLine(c.Vendor);
+                        Console.WriteLine(string.Join(", ", c.Comments.Select(p => p.Key + ": " + p.Value)));
+                    }
+                }
+            }
+        }
+    }
+}
